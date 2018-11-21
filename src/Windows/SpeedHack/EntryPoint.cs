af5baa9b728f70909ef7e5ca12228a2ea35cba8d
Thierry <thierry44@gmail.com>
2018-11-21 10:17:20 -0500

    Add initial Windows speed hack project

---
+++ src/Windows/SpeedHack/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoreHook;
+
+namespace SpeedHack
+{
+    public class EntryPoint : IEntryPoint
+    {
+        public EntryPoint(IContext context, float arg1) { }
+        public void Run(IContext context, float speedMultiplier)
+        {
+
+        }
+    }
+}
