528f6eda28461efb95f96a0c0f07dc672b0c52eb
Thierry <thierry44@gmail.com>
2018-11-21 19:29:07 -0500

    Add WSASend detour template to SocketHook

--- src/Windows/SocketHook/SocketHook.cs
+++ src/Windows/SocketHook/SocketHook.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
 using CoreHook;
 
 namespace SocketHook
@@ -9,6 +12,28 @@ namespace SocketHook
         public void Run(IContext context)
         {
 
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        private delegate SocketError DWSASend(
+               IntPtr socket,
+               IntPtr buffer,
+               Int32 length,
+               out IntPtr numberOfBytesSent,
+               SocketFlags flags,
+               IntPtr overlapped,
+               IntPtr completionRoutine);
+
+        private unsafe SocketError Detour_WsaSend(
+            IntPtr socketHandle,
+            WSABuffer* buffers,
+            int bufferCount,
+            out int bytesTransferred,
+            SocketFlags socketFlags,
+            NativeOverlapped* overlapped,
+            IntPtr completionRoutine)
+        {
+            return Interop.Winsock.WSASend(socketHandle, buffers, bufferCount, out bytesTransferred, socketFlags, overlapped, completionRoutine);
         }
     }
 }
