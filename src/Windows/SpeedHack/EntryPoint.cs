12a31c3a236df3d4a58561f80d5b376d4b69c92d
Thierry <thierry44@gmail.com>
2018-11-21 19:10:30 -0500

    refactor: Update QueryPerformanceCounter and make detour functions private

--- src/Windows/SpeedHack/EntryPoint.cs
+++ src/Windows/SpeedHack/EntryPoint.cs
@@ -48,21 +48,21 @@ namespace SpeedHack
                 LocalHook.GetProcAddress("kernel32.dll", "GetTickCount"),
                 new GetTickCountDel(Detour_GetTickCount),
                 this);
-
+            
             GetTickCount64 = LocalHook.Create(
                 LocalHook.GetProcAddress("kernel32.dll", "GetTickCount64"),
                 new GetTickCount64Del(Detour_GetTickCount64),
                 this);
-
+           
             QueryPerformanceCounter = LocalHook.Create(
                 LocalHook.GetProcAddress("kernel32.dll", "QueryPerformanceCounter"),
                 new QueryPerformanceCounterDel(Detour_QueryPerformanceCounter),
                 this);
-
-            SleepEx = LocalHook.Create(
-                LocalHook.GetProcAddress("kernel32.dll", "SleepEx"),
-                new SleepExDel(Detour_SleepEx),
-                this);
+            
+           SleepEx = LocalHook.Create(
+               LocalHook.GetProcAddress("kernel32.dll", "SleepEx"),
+               new SleepExDel(Detour_SleepEx),
+               this);
 
             // Enable for all threads except the current thread.
             GetTickCount.ThreadACL.SetExclusiveACL(new int[] { 0 });
@@ -71,32 +71,30 @@ namespace SpeedHack
             SleepEx.ThreadACL.SetExclusiveACL(new int[] { 0 });
         }
 
-        internal uint Detour_GetTickCount()
+        private uint Detour_GetTickCount()
         {
             SpeedHack This = (SpeedHack)HookRuntimeInfo.Callback;
             var tickCount = Interop.Kernel32.GetTickCount();
             return (uint)(This._baseTime + ((tickCount - This._baseTime)) * This._acceleration);
         }
 
-        internal ulong Detour_GetTickCount64()
+        private ulong Detour_GetTickCount64()
         {
             SpeedHack This = (SpeedHack)HookRuntimeInfo.Callback;
             var tickCount = Interop.Kernel32.GetTickCount64();
             return (ulong)(This._baseTime64 + ((tickCount - This._baseTime64)) * This._acceleration);
         }
 
-        internal Interop.BOOL Detour_QueryPerformanceCounter(out long performanceCount)
+        private Interop.BOOL Detour_QueryPerformanceCounter(out long performanceCount)
         {
             SpeedHack This = (SpeedHack)HookRuntimeInfo.Callback;
-            if(Interop.Kernel32.QueryPerformanceCounter(out performanceCount) == Interop.BOOL.FALSE)
-            {
-                return Interop.BOOL.FALSE;
-            }
-            performanceCount = (long)(This._basePerformanceCount + ((performanceCount - This._basePerformanceCount)) * This._acceleration);
-            return Interop.BOOL.TRUE;
+            long realPerformanceCount = 0;
+            var result = Interop.Kernel32.QueryPerformanceCounter(out realPerformanceCount);
+            performanceCount = (long)(This._basePerformanceCount + ((realPerformanceCount - This._basePerformanceCount)) * This._acceleration);
+            return result;
         }
 
-        internal uint Detour_SleepEx(uint milliSeconds, Interop.BOOL alertable)
+        private uint Detour_SleepEx(uint milliSeconds, Interop.BOOL alertable)
         {
             SpeedHack This = (SpeedHack)HookRuntimeInfo.Callback;
             return Interop.Kernel32.SleepEx((uint)(milliSeconds / This._acceleration), alertable);
