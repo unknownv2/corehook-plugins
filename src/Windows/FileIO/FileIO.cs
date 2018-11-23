6808a0625338de71ead5e5284ec4a449bc9f3fff
Thierry <thierry44@gmail.com>
2018-11-23 01:17:01 -0500

    feature: Add initial commit for the Windows File IO hooking example plugin

---
+++ src/Windows/FileIO/FileIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CoreHook;
+
+namespace FileIO
+{
+    public class FileIO : IEntryPoint
+    {
+        public FileIO(IContext context) { }
+        
+        public void Run(IContext contex)
+        {
+
+        }
+
+        private int Detour_WriteFile(IntPtr handle,
+            byte[] bytes,
+            int numBytesToWrite,
+            out int numBytesWritten,
+            IntPtr mustBeZero)
+        {
+            return Interop.Kernel32.WriteFile(handle, bytes, numBytesToWrite, out numBytesWritten, mustBeZero);
+        }
+    }
+}
